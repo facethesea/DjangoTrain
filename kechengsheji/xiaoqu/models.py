110e809e2a8cf9310a1d53d5175b85d1afc7a1bd
cly <1273770796@qq.com>
2022-06-28 12:15:28 +0800

    Django模型建模

--- kechengsheji/xiaoqu/models.py
+++ kechengsheji/xiaoqu/models.py
@@ -1,3 +1,10 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class xiaoqu_wailai(models.Model):
+    wailai_id=model.CharField(max_length=12,primary_key=True)
+    name=models.CharField(max_length=12,null=True)
+    age=models.IntegerField()
+    yezhu_id=model.CharField(max_length=12,primary_key=True)
